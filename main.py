7460e39eadfd05b65521e15165fa7e7b708a5d96
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-08-25 23:32:23 +0000

    Redid the workshop to allow users to test their passwords, and give suggestions if there password are easily guessable and styling

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
+from flask import Flask, render_template, request
 import hashlib
 from urllib.request import urlopen
+import random
+import string
+
+app = Flask(__name__)
 
 def readwordlist(url):
     try:
         wordlistfile = urlopen(url).read()
     except Exception as e:
-        print("Hey there was some error while reading the wordlist, error:", e)
-        exit()
+        return f"Error reading wordlist: {e}"
     return wordlistfile
 
-
-def hash(wordlistpassword):
-    result = hashlib.sha1(wordlistpassword.encode())
+def hash(password):
+    result = hashlib.sha1(password.encode())
     return result.hexdigest()
 
+def generate_password_suggestion():
+    """Generate a strong password suggestion."""
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(16))
 
 def bruteforce(guesspasswordlist, actual_password_hash):
     for guess_password in guesspasswordlist:
         if hash(guess_password) == actual_password_hash:
-            print("Hey! your password is:", guess_password,
-                  "\n please change this, it was really easy to guess it (:")
-            exit()
+            suggestions = [generate_password_suggestion() for _ in range(3)]
+            return f"Password cracked: {guess_password}", suggestions
+    return "Password not found in wordlist", []
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = ""
+    suggestions = []
+    if request.method == 'POST':
+        actual_password = request.form['password']
+        actual_password_hash = hash(actual_password)
 
-url = 'https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top12Thousand-probable-v2.txt'
+        url = 'https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top12Thousand-probable-v2.txt'
+        wordlist = readwordlist(url).decode('UTF-8')
+        guesspasswordlist = wordlist.split('\n')
 
-print("Enter your actual passoword and let's guess how easy it is to guess it!\n ex: trying entering 'henry' as a password without the '' ")
+        result, suggestions = bruteforce(guesspasswordlist, actual_password_hash)
 
-while True:
-    actual_password = input()
-    actual_password_hash = hash(actual_password)
-    
-    wordlist = readwordlist(url).decode('UTF-8')
-    guesspasswordlist = wordlist.split('\n')
-    more = input("do you want to add more passwords to the wordlist? (y/n)")
-    
-bruteforce(guesspasswordlist, actual_password_hash)
-    
-print("Hey! I couldn't guess this password, it was not in my wordlist, this is good news! you win (: \n\n ps: You can always import a new wordlist and check whether ur password still passes this bruteforce attack")
+    return render_template('index.html', result=result, suggestions=suggestions)
 
+if __name__ == '__main__':
+    app.run(debug=True)
